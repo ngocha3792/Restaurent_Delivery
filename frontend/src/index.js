30c6157c8dee9e2e10cdcb517ac7cbf62959c2e6
ngocha3792 <ngocha3792@gmail.com>
2025-03-20 21:00:23 +0700

    Update something

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header></Header>
     <App />
+    <Footer></Footer>
   </React.StrictMode>
 );
 
