d131fd0ef599d5d6fdd0adcdbf5745dd9edabec9
ngocha3792 <ngocha3792@gmail.com>
2025-04-16 08:57:09 +0700

    update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios' 
+require("dotenv").config();
 
+require 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api',
     headers: {
         'Content-Type': 'application/json',
     },
